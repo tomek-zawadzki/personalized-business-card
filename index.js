c1e10124104974c10f6d65980e0e256e7d7b21d0
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-05 21:31:03 +0200

    add style for button

--- index.js
+++ index.js
@@ -10,11 +10,15 @@ darkStyle.addEventListener('click', () => {
         root.style.setProperty('--background-color', '#18191a');
         root.style.setProperty('--text-color', '#b0946c');
         root.style.setProperty('--main-background', '#242526');
+        root.style.setProperty('--button-color', '#001a23');
+        root.style.setProperty('--button-background', '#D6D1CD');
         isDark = false;
     } else  {
         root.style.setProperty('--background-color', '#cbeef3');
         root.style.setProperty('--text-color', '#31493C');
         root.style.setProperty('--main-background', '#fff');
+        root.style.setProperty('--button-color', '#D6D1CD');
+        root.style.setProperty('--button-background', '#001a23');
         isDark = true;
     }
     
