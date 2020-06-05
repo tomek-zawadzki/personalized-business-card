3760f0599c72037aac72679f8e2c6c01dac3c8f3
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-05 22:36:38 +0200

    add media queries for mobile

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ darkStyle.addEventListener("click", () => {
     darkStyle.innerHTML = "CIEMNO";
     root.style.setProperty("--background-color", "#18191a");
     root.style.setProperty("--text-color", "#b0946c");
-    root.style.setProperty("--main-background", "#242526");
+    root.style.setProperty("--main-background", "#323336");
     root.style.setProperty("--button-color", "#001a23");
     root.style.setProperty("--button-background", "#D6D1CD");
     isDark = false;
