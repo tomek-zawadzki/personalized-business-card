0a65caaeaf62a0e6538daa228b8ec9c552525183
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-05 21:47:37 +0200

    small fix

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-console.log('Hello bubku')
+console.log("Hello my friend");
 
-
-const darkStyle = document.querySelector('.button--js');
+const darkStyle = document.querySelector(".button--js");
 const root = document.documentElement;
 let isDark = false;
 
-darkStyle.addEventListener('click', () => {
-    if (isDark) {
-        root.style.setProperty('--background-color', '#18191a');
-        root.style.setProperty('--text-color', '#b0946c');
-        root.style.setProperty('--main-background', '#242526');
-        root.style.setProperty('--button-color', '#001a23');
-        root.style.setProperty('--button-background', '#D6D1CD');
-        isDark = false;
-    } else  {
-        root.style.setProperty('--background-color', '#cbeef3');
-        root.style.setProperty('--text-color', '#31493C');
-        root.style.setProperty('--main-background', '#fff');
-        root.style.setProperty('--button-color', '#D6D1CD');
-        root.style.setProperty('--button-background', '#001a23');
-        isDark = true;
-    }
-    
-})
- 
-console.log(darkStyle)
+darkStyle.addEventListener("click", () => {
+  if (isDark) {
+    darkStyle.innerHTML = "CIEMNO";
+    root.style.setProperty("--background-color", "#18191a");
+    root.style.setProperty("--text-color", "#b0946c");
+    root.style.setProperty("--main-background", "#242526");
+    root.style.setProperty("--button-color", "#001a23");
+    root.style.setProperty("--button-background", "#D6D1CD");
+    isDark = false;
+  } else {
+    darkStyle.innerHTML = "JASNO";
+    root.style.setProperty("--background-color", "#cbeef3");
+    root.style.setProperty("--text-color", "#31493C");
+    root.style.setProperty("--main-background", "#fff");
+    root.style.setProperty("--button-color", "#D6D1CD");
+    root.style.setProperty("--button-background", "#001a23");
+    isDark = true;
+  }
+});
 
-
+console.log(darkStyle);
