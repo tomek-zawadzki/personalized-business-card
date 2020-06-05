2e0626a442854bd36fa4059c54677b9e3f6de04c
Tomek Zawadzki <tomekzawadzki1990@gmail.com>
2020-06-05 21:06:09 +0200

    fixed JS code to dark mode

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const darkStyle = document.querySelector('.checkbox--js')
+console.log('Hello bubku')
 
-darkStyle.addEventListener('change', () => {
-    const body = document.querySelector('.body')
-    document.body.classList.toggle('body--js')
+
+const darkStyle = document.querySelector('.button--js');
+const root = document.documentElement;
+let isDark = false;
+
+darkStyle.addEventListener('click', () => {
+    if (isDark) {
+        root.style.setProperty('--background-color', '#18191a');
+        root.style.setProperty('--text-color', '#b0946c');
+        root.style.setProperty('--main-background', '#242526');
+        isDark = false;
+    } else  {
+        root.style.setProperty('--background-color', '#cbeef3');
+        root.style.setProperty('--text-color', '#31493C');
+        root.style.setProperty('--main-background', '#fff');
+        isDark = true;
+    }
+    
 })
-
+ 
 console.log(darkStyle)
-  
 
-let isDark = false
+
